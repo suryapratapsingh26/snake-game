34c582ba236c2c8348eeafb1d35cf3719d79692d
suryapratapsingh26 <suryapratap841239@gmail.com>
2026-09-07 21:50:23 +0530

    Create game board

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
+import './App.css'
 function App() {
-  return <h1>Snake Game</h1>;
+    return (
+      <>
+   <h1 className = "title">Snake Game</h1>
+   <div className = "Board"></div>
+   </>
+    
+    )
 }
 
-export default App;
+export default App
