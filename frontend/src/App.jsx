078c3477e3d6d90a1df9cb2065ee242f70713d28
suryapratapsingh26 <146753809+suryapratapsingh26@users.noreply.github.com>
2026-09-08 16:06:21 +0530

    Create game board (#2)
    * Create game board
    
    * Change class name from uppercase to lowercase
    
    * Fix formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
+import "./App.css";
 function App() {
-  return <h1>Snake Game</h1>;
+  return (
+    <>
+      <h1 className="title">Snake Game</h1>
+      <div className="board"></div>
+    </>
+  );
 }
 
 export default App;
