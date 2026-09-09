73d897a0a34242bc32c50aa9a396b9b14bd5ef55
suryapratapsingh26 <suryapratap841239@gmail.com>
2026-09-09 19:04:32 +0530

    Create snake and implement snake movement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,80 @@
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
+
 function App() {
+  const [snakePosition, setSnakePosition] = useState(154);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [direction, setDirection] = useState("right");
+
+  const directionRef = useRef("right");
+
+  const handleKeyDown = (event) => {
+    if (!gameStarted) return;
+
+    if (
+      (event.key === "ArrowUp" || event.key === "w") &&
+      directionRef.current !== "down"
+    ) {
+      directionRef.current = "up";
+      setDirection("up");
+    } else if (
+      (event.key === "ArrowDown" || event.key === "s") &&
+      directionRef.current !== "up"
+    ) {
+      directionRef.current = "down";
+      setDirection("down");
+    } else if (
+      (event.key === "ArrowLeft" || event.key === "a") &&
+      directionRef.current !== "right"
+    ) {
+      directionRef.current = "left";
+      setDirection("left");
+    } else if (
+      (event.key === "ArrowRight" || event.key === "d") &&
+      directionRef.current !== "left"
+    ) {
+      directionRef.current = "right";
+      setDirection("right");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [gameStarted]);
+
+  useEffect(() => {
+    if (!gameStarted) return;
+
+    const interval = setInterval(() => {
+      setSnakePosition((position) => {
+        if (directionRef.current === "up") return position - 20;
+        if (directionRef.current === "down") return position + 20;
+        if (directionRef.current === "left") return position - 1;
+        return position + 1;
+      });
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, [gameStarted]);
+
   return (
     <>
       <h1 className="title">Snake Game</h1>
-      <div className="board"></div>
-      <button className="button">Play</button>
+
+      <div className="board">
+        {Array.from({ length: 20 * 15 }).map((_, index) => (
+          <div
+            className={index === snakePosition ? "cell snake" : "cell"}
+            key={index}
+          ></div>
+        ))}
+      </div>
+
+      <button className="button" onClick={() => setGameStarted(true)}>
+        Play
+      </button>
     </>
   );
 }
