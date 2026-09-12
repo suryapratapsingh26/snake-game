d326334c39be028f872f8cb533ed0d393d0bb349
suryapratapsingh26 <suryapratap841239@gmail.com>
2026-09-12 17:46:23 +0530

    Add comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,15 +20,25 @@ const getNextPosition = (position, direction) => {
   let nextRow = row;
   let nextColumn = col;
 
+  // If the snake head is at the top row and user presses UP then
+  // the snake head would come out of the bottom other wise it will move upward
+  //Hear BOARD_HEIGHT-1 is used for denoting last row i.e max height
   if (direction === DIRECTIONS.UP) {
     nextRow = row == 0 ? BOARD_HEIGHT - 1 : row - 1;
   }
+  // If the snake head is at the bottom of the board and player presses DOWN then
+  // the snake head comes out of the top of the board otherwise it would move downward
   if (direction === DIRECTIONS.DOWN) {
     nextRow = row == BOARD_HEIGHT - 1 ? 0 : row + 1;
   }
+  // If the snake head is at the last column and user presses RIGHT then
+  // the snake head would come out of the 1st column
+  // Here BOARD_WIDTH-1 is used for denoting last column
   if (direction === DIRECTIONS.RIGHT) {
     nextColumn = col == BOARD_WIDTH - 1 ? 0 : col + 1;
   }
+  // If the snake head is at the 1st column and user presses LEFT then snake
+  // head would come out of last column
   if (direction === DIRECTIONS.LEFT) {
     nextColumn = col == 0 ? BOARD_WIDTH - 1 : col - 1;
   }
